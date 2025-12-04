1be86df8b828544bcf5cba075b33d1213d721768
Irman Faqrizal <ifaqriza@ptb-04021020.inria.fr>
2025-12-04 13:27:43 +0100

    experiments

--- experiments/experiments.sh
+++ experiments/experiments.sh
@@ -1,8 +1,8 @@
 ifModel=simple.if
 traces=traces
-cd ../ptsv-app
-mvn clean package
-cp target/ptsv.jar ../experiments/ptsv.jar
-cd ../experiments
+# cd ../ptsv-app
+# mvn clean package
+# cp target/ptsv.jar ../experiments/ptsv.jar
+# cd ../experiments
 java -cp ptsv.jar com.ptsv.app.App $ifModel
 java -cp ptsv.jar com.ptsv.app.App $ifModel $traces
