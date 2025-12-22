9c7460c527457fb80be5b807b9c3e1cd04906cb2
Irman Faqrizal <ifaqriza@ptb-04021020.inria.fr>
2025-12-22 10:13:24 +0100

    error

--- experiments/example/test.sh
+++ experiments/example/test.sh
@@ -1,6 +1,6 @@
-# cd ../../ptsv-app
-# mvn clean package
-# cp target/ptsv.jar ../experiments/simple/ptsv.jar
-# cd ../experiments/simple
+cd ../../ptsv-app
+mvn clean package
+cp target/ptsv.jar ../experiments/example/ptsv.jar
+cd ../experiments/example
 java -cp ptsv.jar com.ptsv.app.App simple.if
-java -cp ptsv.jar com.ptsv.app.App simple.if traces
+# java -cp ptsv.jar com.ptsv.app.App simple.if traces
