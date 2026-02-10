aa1298e6884158eab4f21771fbae00fb66accc37
Irman Faqrizal <ifaqriza@ptb-04021020.inria.fr>
2026-02-10 17:44:54 +0100

    correction

--- experiments/simple/test.sh
+++ experiments/simple/test.sh
@@ -2,7 +2,7 @@ cd ../../ptsv-app
 # mvn clean package
 cp target/ptsv.jar ../experiments/simple/ptsv.jar
 cd ../experiments/simple
-java -cp ptsv.jar com.ptsv.app.App simple.if -trace traces
+java -cp ptsv.jar com.ptsv.app.App simple.if
 
 #### Uncomment below to use trace injection
 # java -cp ptsv.jar com.ptsv.app.App simple.if -trace traces
